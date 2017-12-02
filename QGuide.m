00783bbe16b2bbf029037948e41f2473f40627ef
Dong Hur <dhur@college.harvard.edu>
2017-12-02 15:50:44 -0500

    more files

--- QGuide.m
+++ QGuide.m
@@ -25,7 +25,7 @@ inst_access = data(:, 7);     % Instructor Accessibility
 
 deltaY = y2-y1;     % Change in enrollment
 % X=[overall_rating workload_hours recommend inst_overall inst_access];
-X=[workload_hours recommend inst_overall inst_access];
+X=[workload_hours recommend];
 % plots the data
 [~, ax] = plotmatrix([deltaY X])
 % ax(1,1).YLabel.String='Change in Enrollment'; 
@@ -38,8 +38,6 @@ X=[workload_hours recommend inst_overall inst_access];
 ax(1,1).YLabel.String='Change in Enrollment'; 
 ax(2,1).YLabel.String='Workload Hours'; 
 ax(3,1).YLabel.String='Recommendation'; 
-ax(4,1).YLabel.String='Instructor Overall'; 
-ax(5,1).YLabel.String='Instructor Accessibility'; 
 
 % 
 % ax(6,1).XLabel.String='Change in Enrollment'; 
@@ -50,11 +48,9 @@ ax(5,1).YLabel.String='Instructor Accessibility';
 % ax(6,6).XLabel.String='Instructor Accessibility'; 
 
 
-ax(5,1).XLabel.String='Change in Enrollment'; 
-ax(5,2).XLabel.String='Workload Hours'; 
-ax(5,3).XLabel.String='Recommendation'; 
-ax(5,4).XLabel.String='Instructor Overall'; 
-ax(5,5).XLabel.String='Instructor Accessibility'; 
+ax(3,1).XLabel.String='Change in Enrollment'; 
+ax(3,2).XLabel.String='Workload Hours'; 
+ax(3,3).XLabel.String='Recommendation'; 
 
 % fits linear regression
 fitlm(X, deltaY)
