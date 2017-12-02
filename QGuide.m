ffbcbb5d0c7dc5a2712e434b33b7db8cfce7b5cc
Dong Hur <dhur@college.harvard.edu>
2017-12-02 17:03:36 -0500

    more files

--- QGuide.m
+++ QGuide.m
@@ -13,8 +13,8 @@
 clc
 clear
 
-data=csvread('QGuideData3.csv', 1, 1);
-
+% data=csvread('QGuideData3.csv', 1, 1);
+data=csvread('QGuideDataBigClass.csv', 1, 1);
 y1 = data(:,1);               % next year student enrollment
 y2 = data(:,2);               % current year student enrollment
 overall_rating = data(:,3);   % Overall Rating
@@ -22,35 +22,40 @@ workload_hours = data(:,4);   % Workload Hours
 recommend = data(:,5);        % Recommend
 inst_overall = data(:,6);     % Instructor Overall
 inst_access = data(:, 7);     % Instructor Accessibility
+elective = data(:, 8);
+requirement = data(:, 9);
 
-deltaY = y2-y1;     % Change in enrollment
-% X=[overall_rating workload_hours recommend inst_overall inst_access];
-X=[workload_hours recommend];
+deltaY = y1-y2;     % Change in enrollment
+percentInc = (y1-y2)./y2;
+ele_req = elective+requirement;
+X=[overall_rating workload_hours recommend inst_overall inst_access];
+% X=[workload_hours recommend, requirement];
 % plots the data
-[~, ax] = plotmatrix([deltaY X])
-% ax(1,1).YLabel.String='Change in Enrollment'; 
-% ax(2,1).YLabel.String='Overall Rating'; 
-% ax(3,1).YLabel.String='Workload Hours'; 
-% ax(4,1).YLabel.String='Recommendation'; 
-% ax(5,1).YLabel.String='Instructor Overall'; 
-% ax(6,1).YLabel.String='Instructor Accessibility'; 
-
+[~, ax] = plotmatrix([percentInc X])
 ax(1,1).YLabel.String='Change in Enrollment'; 
-ax(2,1).YLabel.String='Workload Hours'; 
-ax(3,1).YLabel.String='Recommendation'; 
+ax(2,1).YLabel.String='Overall Rating'; 
+ax(3,1).YLabel.String='Workload Hours'; 
+ax(4,1).YLabel.String='Recommendation'; 
+ax(5,1).YLabel.String='Instructor Overall'; 
+ax(6,1).YLabel.String='Instructor Accessibility'; 
+
+% ax(1,1).YLabel.String='Change in Enrollment'; 
+% ax(2,1).YLabel.String='Workload Hours'; 
+% ax(3,1).YLabel.String='Recommendation'; 
+% ax(4,1).YLabel.String='Requirement'; 
+% 
+ax(6,1).XLabel.String='Change in Enrollment'; 
+ax(6,2).XLabel.String='Overall Rating'; 
+ax(6,3).XLabel.String='Workload Hours'; 
+ax(6,4).XLabel.String='Recommendation'; 
+ax(6,5).XLabel.String='Instructor Overall'; 
+ax(6,6).XLabel.String='Instructor Accessibility'; 
 
 % 
-% ax(6,1).XLabel.String='Change in Enrollment'; 
-% ax(6,2).XLabel.String='Overall Rating'; 
-% ax(6,3).XLabel.String='Workload Hours'; 
-% ax(6,4).XLabel.String='Recommendation'; 
-% ax(6,5).XLabel.String='Instructor Overall'; 
-% ax(6,6).XLabel.String='Instructor Accessibility'; 
-
-
-ax(3,1).XLabel.String='Change in Enrollment'; 
-ax(3,2).XLabel.String='Workload Hours'; 
-ax(3,3).XLabel.String='Recommendation'; 
+% ax(4,1).XLabel.String='Change in Enrollment'; 
+% ax(4,2).XLabel.String='Workload Hours'; 
+% ax(4,3).XLabel.String='Recommendation'; 
+% ax(4,4).XLabel.String='Requirement'; 
 
 % fits linear regression
 fitlm(X, deltaY)
